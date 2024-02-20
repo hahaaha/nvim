9e771935dd036446bf938b6569c156ef1438ea9d
haha <390519764@qq.com>
2024-02-20 20:43:41 +0800

    refactor: 增加gd快捷键

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,16 +5,12 @@ vim.keymap.set("n", "<C-p>", ":Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>f", ":Telescope live_grep<cr>")
 -- vim.keymap.set('n', '<C-p>', builtin.find_files, silent)
 
---
--- map("n", "<D-b", "NvimTreeToggle<CR>")
---
-
 -- bufferline
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 
---vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>ff', builtin.find_files,{})
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	end,
+})
