73e63d9b8d011dee3a1d59e8fd7c870c6679d94c
haha <390519764@qq.com>
2024-02-21 23:20:45 +0800

    refactor: 增加面包屑

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,17 @@ autocmd("BufWritePost", {
 	group = "__formatter__",
 	command = ":FormatWrite",
 })
+
+-- UI
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+local lspui_ok, lspui = pcall(require, 'lspconfig.ui.windows')
+if not lspui_ok then
+  return
+end
