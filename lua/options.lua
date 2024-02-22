f32839ca28a72c7f661ef5497055bcc807079b0b
haha <390519764@qq.com>
2024-02-22 23:50:52 +0800

    feat: 增加配色

--- lua/options.lua
+++ lua/options.lua
@@ -20,11 +20,11 @@ autocmd("BufWritePost", {
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-local lspui_ok, lspui = pcall(require, 'lspconfig.ui.windows')
+local lspui_ok, lspui = pcall(require, "lspconfig.ui.windows")
 if not lspui_ok then
-  return
+	return
 end
