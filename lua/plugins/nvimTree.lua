19e629adcb4bb64a826d192ceca552ccac69e068
haha <390519764@qq.com>
2024-02-27 14:08:16 +0800

    refactor: 更新插件版本以及调整nvim-tree配置

--- lua/plugins/nvimTree.lua
+++ lua/plugins/nvimTree.lua
@@ -1,15 +1,28 @@
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
-		version = "*",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("nvim-tree").setup({
+				hijack_cursor = true,
 				update_focused_file = {
 					enable = true,
+					update_cwd = true,
 				},
 				filters = {
 					dotfiles = true,
+				},
+				view = {
+					relativenumber = true,
+					number = true,
+				},
+				renderer = {
+					indent_markers = {
+						enable = true,
+					},
+				},
+				diagnostics = {
+					enable = true,
 				},
 			})
 		end,
