6a8fea60dfa6ec2b488ed3c73a351175812d8ec5
haha <390519764@qq.com>
2024-03-02 14:59:06 +0800

    refactor: 增加css语法提示

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		local lspconfig = require("lspconfig")
+
 		lspconfig["lua_ls"].setup({
 			settings = {
 				Lua = {
@@ -14,8 +15,15 @@ return {
 		lspconfig["html"].setup({})
 		lspconfig["tsserver"].setup({})
 		lspconfig["hls"].setup({})
-		lspconfig["cssls"].setup({})
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
+		lspconfig["cssls"].setup({
+			capabilities = capabilities,
+		})
+
 		lspconfig["jsonls"].setup({})
 		lspconfig["bashls"].setup({})
+		lspconfig["cssmodules_ls"].setup({})
 	end,
 }
