85e9251002623cdf6f6e2dc1c80d947e01cf853d
haha <390519764@qq.com>
2024-03-10 23:34:32 +0800

    refactor: git sign config调整

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,26 +1,26 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		messages = { view = "mini" },
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
-			signature = {
-				-- enabled = false,
-			},
-		},
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        messages = { view = "mini" },
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            },
+            signature = {
+                -- enabled = false,
+            },
+        },
+    },
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+    },
 }
