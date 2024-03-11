1d25370988ac3b29d30c27f5d832e5f7410a6b6a
haha <390519764@qq.com>
2024-03-11 14:48:33 +0800

    refactor: 优化comment在jsx/tsx下的表现

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,8 @@
--- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
     'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
-    },
-    lazy = false,
+    config = function()
+        require('Comment').setup({
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        })
+    end
 }
-
