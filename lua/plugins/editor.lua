705386c1617678ecf1d8d38c2d9014eb6b8be760
haha <390519764@qq.com>
2025-03-21 17:02:17 +0800

    feat: add outline

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,6 +7,17 @@ return {
             require('tiny-inline-diagnostic').setup()
         end
     },
+    {
+        "hedyhli/outline.nvim",
+        lazy = true,
+        cmd = { "Outline", "OutlineOpen" },
+        keys = { -- Example mapping to toggle outline
+            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        },
+        opts = {
+            -- Your setup opts here
+        },
+    },
 
     -- {
     --     'nvimdev/dashboard-nvim',
